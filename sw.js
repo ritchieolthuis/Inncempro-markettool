245122a27c4e29da02f5237ae677175513c6f208
Ritchie O <ritchie@Ritchies-MacBook-Pro.local>
2026-07-22 21:30:56 +0200

    Updates

--- sw.js
+++ sw.js
@@ -87,7 +87,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "77de9ae56cb5dff9abab29d14ba3cc49"
   }, {
     "url": "index.html",
-    "revision": "9b0fe7fec5cc2443f3833cf18eb99be7"
+    "revision": "4aeca036119e8852e779ece8155a644b"
   }, {
     "url": "icon-maskable-512.png",
     "revision": "aafb99953f9a01fa18ee7d4cfc99a645"
@@ -113,7 +113,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon.png",
     "revision": "0214a924cfd2a2be5adb183f567e37c5"
   }, {
-    "url": "assets/index-Dfa9ep--.js",
+    "url": "assets/index-y1hG-gGm.js",
     "revision": null
   }, {
     "url": "assets/index-BWJgyTSR.css",
